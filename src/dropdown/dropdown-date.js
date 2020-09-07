3dd9a93758a672a998ec2bec8c217e1e692de28a
ddenis87 <dizdok@yandex.ru>
2020-09-08 06:38:08 +0900

    build date

--- src/dropdown/dropdown-date.js
+++ src/dropdown/dropdown-date.js
@@ -5,5 +5,8 @@ import './__date/dropdown-date__date';
 export default class DropdownDate extends Dropdown {
     constructor(idDropdown) {
       super(idDropdown);
+      console.log(idDropdown);
+      document.getElementById(idDropdown + 'Slider').classList.add('dropdown__slider-date');
+      document.getElementById(idDropdown + 'Control').classList.add('dropdown__control-date');
     }
 }
