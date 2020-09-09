68ba2f6bb5697b9bbcc714324b8094e218cdfd3a
ddenis87 <dizdok@yandex.ru>
2020-09-09 20:28:30 +0900

    Добавлен прогресбар к слайдеру, добавлена функция плавного перемещения диапазона

--- src/slider-bar/__range/slider-bar__range.js
+++ src/slider-bar/__range/slider-bar__range.js
@@ -4,7 +4,9 @@ export default class SliderBarRange {
   constructor(option) {
     this.idSliderBar = option.id;
     document.getElementById(option.id + 'Body').addEventListener('click', () => this.clickSliderBody(event));
-    this.moveProgressBar()
+    document.getElementById(option.id + 'From').addEventListener('mousemove', () => this.sliderMoveFrom(event)); 
+    document.getElementById(option.id + 'To').addEventListener('mousemove', () => this.sliderMoveTo(event)); 
+    this.moveProgressBar();
   }
 
   clickSliderBody(event) {
@@ -29,5 +31,20 @@ export default class SliderBarRange {
 
     progressBar.style.left = fromCoordinat.x - parentCoordinat.x + 'px';
     progressBar.style.width = (toCoordinat.x - fromCoordinat.x) + 'px';
+  }
+
+  sliderMoveFrom(event) {
+    let parentCoordinat = document.getElementById(this.idSliderBar + "Body").getBoundingClientRect();
+    if (event.which == 1) {
+      document.getElementById(this.idSliderBar + "From").style.left = event.clientX - (parentCoordinat.x + 10) + 'px';
+    }
+    this.moveProgressBar();
+  }
+  sliderMoveTo(event) {
+    let parentCoordinat = document.getElementById(this.idSliderBar + "Body").getBoundingClientRect();
+    if (event.which == 1) {
+      document.getElementById(this.idSliderBar + "To").style.left = event.clientX - (parentCoordinat.x + 10) + 'px';
+    }
+    this.moveProgressBar();
   }
 }
--- src/slider-bar/__slider/slider-bar__slider.js
+++ src/slider-bar/__slider/slider-bar__slider.js
@@ -5,11 +5,25 @@ export default class SliderBarSlider {
     this.idSliderBar = option.id;
     document.getElementById(option.id + 'Body').addEventListener('click', () => this.clickSliderBody(event));
     document.getElementById(option.id + 'Slider').addEventListener('mousemove', () => this.sliderMove(event)); 
+
+    this.moveProgressBar();
   }
 
   clickSliderBody(event) {
     let parentCoordinat = document.getElementById(this.idSliderBar + "Body").getBoundingClientRect();
     document.getElementById(this.idSliderBar + "Slider").style.left = event.clientX - (parentCoordinat.x + 10) + 'px';
+    this.moveProgressBar();
+  }
+
+  moveProgressBar() {
+    let parentCoordinat = document.getElementById(this.idSliderBar + 'Body').getBoundingClientRect();
+    let sliderCoordinat = document.getElementById(this.idSliderBar + 'Slider').getBoundingClientRect();
+    // let toCoordinat = document.getElementById(this.idSliderBar + 'To').getBoundingClientRect();
+    let progressBar = document.getElementById(this.idSliderBar + 'ProgressBar');
+
+    // progressBar.style.left = fromCoordinat.x - parentCoordinat.x + 'px';
+    progressBar.style.width = sliderCoordinat.x - parentCoordinat.x + 'px';
+    // progressBar.style.width = (toCoordinat.x - fromCoordinat.x) + 'px';
   }
 
   sliderMove(event) {
@@ -17,5 +31,6 @@ export default class SliderBarSlider {
     if (event.which == 1) {
       document.getElementById(this.idSliderBar + "Slider").style.left = event.clientX - (parentCoordinat.x + 10) + 'px';
     }
+    this.moveProgressBar();
   }
 }
